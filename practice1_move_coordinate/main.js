1ce5633bcc4bf7c9a7ab80fa998dbe123e161098
ksy <ksy8230@hanmail.net>
2021-03-05 08:18:23 +0900

    css 속성 변경해서 성능 개선

--- practice1_move_coordinate/main.js
+++ practice1_move_coordinate/main.js
@@ -5,12 +5,20 @@
   const verticalLine = document.querySelector('.vertical-line');
   const horizonLine = document.querySelector('.horizon-line');
 
-  window.addEventListener('mousemove', (e) => {
-    element.style.top = `${e.pageY}px`;
-    element.style.left = `${e.pageX}px`;
-    verticalLine.style.left = `${e.pageX}px`;
-    horizonLine.style.top = `${e.pageY}px`;
-    elementx.textContent = `${e.pageX}px`;
-    elementy.textContent = `${e.pageY}px`;
+  window.addEventListener('load', () => {
+    window.document.addEventListener('mousemove', (e) => {
+      const x = e.clientX;
+      const y = e.clientY;
+      const targetRect = element.getBoundingClientRect();
+      const targetHalfWidth = targetRect.width / 2;
+      const targetHalfHeight = targetRect.height / 2;
+      element.style.transform = `translate(${x - targetHalfWidth}px, ${y - targetHalfHeight}px)`;
+
+      verticalLine.style.transform = `translateX(${x}px)`;
+      horizonLine.style.transform = `translateY(${y}px)`;
+
+      elementx.textContent = `${x}px`;
+      elementy.textContent = `${y}px`;
+    });
   });
 }
